69f2fbfef3094f2b00fbe00b331ea0d97f564751
goto <kenta.goto.1228@gmail.com>
2023-01-01 00:37:31 +0900

    feat: WIP add EC2 client

--- pkg/client/ec2.go
+++ pkg/client/ec2.go
@@ -1,1 +1,36 @@
 package client
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
+type Ec2 struct {
+	client *ec2.Client
+}
+
+func NewEC2Client(config aws.Config) *Ec2 {
+	ec2Client := ec2.NewFromConfig(config)
+
+	return &Ec2{
+		client: ec2Client,
+	}
+}
+
+func (ec2Client *Ec2) DescribeRegions(ctx context.Context) ([]string, error) {
+	outputRegions := []string{}
+	input := &ec2.DescribeRegionsInput{}
+
+	output, err := ec2Client.client.DescribeRegions(ctx, input)
+	if err != nil {
+		return outputRegions, err
+	}
+
+	for _, region := range output.Regions {
+		outputRegions = append(outputRegions, *region.RegionName)
+	}
+
+	return outputRegions, nil
+}

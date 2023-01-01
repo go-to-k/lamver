24091b7509da80e0b0acb6e5cadb5e3b4e81ab69
goto <kenta.goto.1228@gmail.com>
2023-01-01 18:36:32 +0900

    chore: change name of receivers in clients

--- pkg/client/ec2.go
+++ pkg/client/ec2.go
@@ -7,23 +7,23 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
-type Ec2 struct {
+type EC2 struct {
 	client *ec2.Client
 }
 
-func NewEc2Client(config aws.Config) *Ec2 {
+func NewEC2Client(config aws.Config) *EC2 {
 	ec2Client := ec2.NewFromConfig(config)
 
-	return &Ec2{
+	return &EC2{
 		client: ec2Client,
 	}
 }
 
-func (ec2Client *Ec2) DescribeRegions(ctx context.Context) ([]string, error) {
+func (c *EC2) DescribeRegions(ctx context.Context) ([]string, error) {
 	outputRegions := []string{}
 	input := &ec2.DescribeRegionsInput{}
 
-	output, err := ec2Client.client.DescribeRegions(ctx, input)
+	output, err := c.client.DescribeRegions(ctx, input)
 	if err != nil {
 		return outputRegions, err
 	}

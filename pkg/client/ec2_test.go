622bb4e560d106b00e658d44954f2af02c9ec89f
goto <kenta.goto.1228@gmail.com>
2023-01-03 15:33:26 +0900

    test: add base test files

---
+++ pkg/client/ec2_test.go
@@ -0,0 +1,63 @@
+package client
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
+func TestNewEC2Client(t *testing.T) {
+	type args struct {
+		config aws.Config
+	}
+	tests := []struct {
+		name string
+		args args
+		want *EC2
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewEC2Client(tt.args.config); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewEC2Client() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEC2_DescribeRegions(t *testing.T) {
+	type fields struct {
+		client *ec2.Client
+	}
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &EC2{
+				client: tt.fields.client,
+			}
+			got, err := c.DescribeRegions(tt.args.ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EC2.DescribeRegions() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("EC2.DescribeRegions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

1dae2d64c5d6880dfeeb33deb0ffee16b627992f
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-01-03 22:54:09 +0900

    test: TestCreateFunctionMap

--- internal/action/action_test.go
+++ internal/action/action_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	lambdaTypes "github.com/aws/aws-sdk-go-v2/service/lambda/types"
 	"github.com/golang/mock/gomock"
 )
 
@@ -112,19 +113,117 @@ func TestGetAllRegionsAndRuntime(t *testing.T) {
 
 func TestCreateFunctionMap(t *testing.T) {
 	type args struct {
-		input *CreateFunctionMapInput
+		ctx           context.Context
+		targetRegions []string
+		targetRuntime []string
+		keyword       string
 	}
+	ctx := context.Background()
+
 	tests := []struct {
-		name    string
-		args    args
-		want    map[string]map[string][][]string
-		wantErr bool
+		name                          string
+		args                          args
+		prepareMockAWSConfigCreatorFn func(m *client.MockAWSConfigCreator)
+		prepareMockLambdaCreatorFn    func(m *client.MockLambdaCreator, c *client.MockLambdaClient)
+		want                          map[string]map[string][][]string
+		wantErr                       bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "CreateFunctionMap success",
+			args: args{
+				ctx:           ctx,
+				targetRegions: []string{"ap-northeast-1", "us-east-1", "us-east-2"},
+				targetRuntime: []string{"nodejs18.x", "nodejs"},
+				keyword:       "",
+			},
+			prepareMockAWSConfigCreatorFn: func(m *client.MockAWSConfigCreator) {
+				m.EXPECT().Create(ctx, "ap-northeast-1").Return(
+					&client.AWSConfig{
+						Config: aws.Config{},
+					}, nil,
+				).AnyTimes()
+				m.EXPECT().Create(ctx, "us-east-1").Return(
+					&client.AWSConfig{
+						Config: aws.Config{},
+					}, nil,
+				).AnyTimes()
+				m.EXPECT().Create(ctx, "us-east-2").Return(
+					&client.AWSConfig{
+						Config: aws.Config{},
+					}, nil,
+				).AnyTimes()
+			},
+			prepareMockLambdaCreatorFn: func(m *client.MockLambdaCreator, c *client.MockLambdaClient) {
+				c.EXPECT().ListFunctions(ctx).Return(
+					[]lambdaTypes.FunctionConfiguration{
+						{
+							FunctionName: aws.String("function1"),
+							Runtime:      lambdaTypes.RuntimeNodejs,
+							LastModified: aws.String("2022-12-21T09:47:43.728+0000"),
+						},
+						{
+							FunctionName: aws.String("function2"),
+							Runtime:      lambdaTypes.RuntimeGo1x,
+							LastModified: aws.String("2022-12-21T09:47:43.728+0000"),
+						},
+						{
+							FunctionName: aws.String("function3"),
+							Runtime:      lambdaTypes.RuntimeNodejs18x,
+							LastModified: aws.String("2022-12-22T09:47:43.728+0000"),
+						},
+					}, nil,
+				).AnyTimes()
+				m.EXPECT().Create(aws.Config{}).Return(
+					c,
+				).AnyTimes()
+			},
+			want: map[string]map[string][][]string{
+				"nodejs": {
+					"ap-northeast-1": {
+						[]string{"function1", "2022-12-21T09:47:43.728+0000"},
+					},
+					"us-east-1": {
+						[]string{"function1", "2022-12-21T09:47:43.728+0000"},
+					},
+					"us-east-2": {
+						[]string{"function1", "2022-12-21T09:47:43.728+0000"},
+					},
+				},
+				"nodejs18.x": {
+					"ap-northeast-1": {
+						[]string{"function3", "2022-12-22T09:47:43.728+0000"},
+					},
+					"us-east-1": {
+						[]string{"function3", "2022-12-22T09:47:43.728+0000"},
+					},
+					"us-east-2": {
+						[]string{"function3", "2022-12-22T09:47:43.728+0000"},
+					},
+				},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := CreateFunctionMap(tt.args.input)
+			ctrl := gomock.NewController(t)
+			awsConfigMock := client.NewMockAWSConfigCreator(ctrl)
+			lambdaFactoryMock := client.NewMockLambdaCreator(ctrl)
+			lambdaClientMock := client.NewMockLambdaClient(ctrl)
+
+			tt.prepareMockAWSConfigCreatorFn(awsConfigMock)
+			tt.prepareMockLambdaCreatorFn(lambdaFactoryMock, lambdaClientMock)
+
+			input := &CreateFunctionMapInput{
+				Ctx:              tt.args.ctx,
+				TargetRegions:    tt.args.targetRegions,
+				TargetRuntime:    tt.args.targetRuntime,
+				Keyword:          tt.args.keyword,
+				AWSConfigFactory: awsConfigMock,
+				LambdaFactory:    lambdaFactoryMock,
+			}
+
+			got, err := CreateFunctionMap(input)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CreateFunctionMap() error = %v, wantErr %v", err, tt.wantErr)
 				return

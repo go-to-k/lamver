4cf633de4ed1405fedc64d8e5af52d47b593ed63
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-01-03 20:13:17 +0900

    test: add mocks and create ec2_test

--- pkg/client/ec2_test.go
+++ pkg/client/ec2_test.go
@@ -2,20 +2,37 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"reflect"
 	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/golang/mock/gomock"
 )
 
 func TestNewEC2(t *testing.T) {
 	type args struct {
 		client EC2SDKClient
 	}
+	ctrl := gomock.NewController(t)
+	mock := NewMockEC2SDKClient(ctrl)
+
 	tests := []struct {
 		name string
 		args args
 		want *EC2
 	}{
-		// TODO: Add test cases.
+		{
+			name: "NewEC2",
+			args: args{
+				client: mock,
+			},
+			want: &EC2{
+				client: mock,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -27,25 +44,86 @@ func TestNewEC2(t *testing.T) {
 }
 
 func TestEC2_DescribeRegions(t *testing.T) {
-	type fields struct {
-		client EC2SDKClient
-	}
 	type args struct {
 		ctx context.Context
 	}
+	ctx := context.Background()
+
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    []string
-		wantErr bool
+		name          string
+		args          args
+		prepareMockFn func(m *MockEC2SDKClient)
+		want          []string
+		wantErr       bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "DescribeRegions success",
+			args: args{
+				ctx: ctx,
+			},
+			prepareMockFn: func(m *MockEC2SDKClient) {
+				m.EXPECT().DescribeRegions(ctx, &ec2.DescribeRegionsInput{}).Return(
+					&ec2.DescribeRegionsOutput{
+						Regions: []types.Region{
+							{
+								RegionName: aws.String("ap-northeast-1"),
+							},
+							{
+								RegionName: aws.String("us-east-1"),
+							}},
+					}, nil)
+			},
+			want: []string{
+				"ap-northeast-1",
+				"us-east-1",
+			},
+			wantErr: false,
+		},
+		{
+			name: "DescribeRegions sorted success",
+			args: args{
+				ctx: ctx,
+			},
+			prepareMockFn: func(m *MockEC2SDKClient) {
+				m.EXPECT().DescribeRegions(ctx, &ec2.DescribeRegionsInput{}).Return(
+					&ec2.DescribeRegionsOutput{
+						Regions: []types.Region{
+							{
+								RegionName: aws.String("us-east-1"),
+							},
+							{
+								RegionName: aws.String("ap-northeast-1"),
+							}},
+					}, nil)
+			},
+			want: []string{ // sort by region name
+				"ap-northeast-1",
+				"us-east-1",
+			},
+			wantErr: false,
+		},
+		{
+			name: "DescribeRegions fail",
+			args: args{
+				ctx: ctx,
+			},
+			prepareMockFn: func(m *MockEC2SDKClient) {
+				m.EXPECT().DescribeRegions(ctx, &ec2.DescribeRegionsInput{}).Return(
+					nil, fmt.Errorf("DescribeRegionsError"))
+			},
+			want:    []string{},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			mock := NewMockEC2SDKClient(ctrl)
+
+			tt.prepareMockFn(mock)
+
 			c := &EC2{
-				client: tt.fields.client,
+				client: mock,
 			}
 			got, err := c.DescribeRegions(tt.args.ctx)
 			if (err != nil) != tt.wantErr {
